32e9b6d01f928cc2e71bb358f9c5fc94cafbc07c
Filip <filip.gadiuta@gmail.com>
2014-05-06 01:08:26 +0300

    PEP8

--- tennis.py
+++ tennis.py
@@ -1,28 +1,33 @@
 import random
 
+
 class Player(object):
-    '''
+    """
     Player has a name and a skill indicator from 0.0 to 1.0
-    '''
+    """
     def __init__(self, name, skill):
-        assert skill >= 0 and skill <= 1, "skill should be from 0 to 1"
+        assert 0 <= skill <= 1, "skill should be from 0 to 1"
         self.name = name
         self.skill = float(skill)
+
     def get_skill(self):
         return self.skill
+
     def get_name(self):
         return self.name
 
+
 class Score(object):
-    def __init__(self, balls, diff, min_games, max_games, games_diff, min_sets):
-        '''
+    def __init__(self, balls, diff, min_games, max_games, games_diff,
+                 min_sets):
+        """
         balls - min. number of balls in game (15-30-40 means 3)
         diff - difference needed to win a game (usually 2)
         min_games - max. number of games required to win a set
         max_games - max. number of games required to win a set
         games_diff - games diff required to win if less than max_games
         min_sets - min. number of sets to win a match
-        '''
+        """
         assert balls > 0, "balls > 0 "
         assert diff > 0, "diff > 0 "
         assert min_games > 0, "min_games > 0 "
@@ -39,17 +44,19 @@ class Score(object):
 #Formula to simulate a match in five sets (three needed to win)
 #classic_score = Score(3, 2, 6, 7, 2, 3)
 
-def ball_result(player1, player2, std1 = 0.5, std2 = 0.2, boundary = 1.5):
-    '''
+
+def ball_result(player1, player2, std1=0.5, std2=0.2, boundary=1.5):
+    """
     The result (res) depends mainly on both players' skills and some random
     noise (random.gauss(m, mu)).
     If res falls outside [-0.75, 0.75], then one player scores.
     Otherwise, players score depending on their cumulative skills (prob_error).
     If they are good players, errors are rarer.
     
-    '''
+    """
     #res = player1.get_skill() - player2.get_skill() + random.gauss(0,std1)
-    res = player1.get_skill() - player2.get_skill() + (random.random()-0.5)*std1
+    res = player1.get_skill() - player2.get_skill() + \
+        (random.random()-0.5)*std1
     if res < -boundary:
         return 2
     elif res > boundary:
@@ -60,17 +67,18 @@ def ball_result(player1, player2, std1 = 0.5, std2 = 0.2, boundary = 1.5):
             return 0
         else:
             #divide prob_error proportionally to skills
-            if random.random() < (player1.get_skill() + random.gauss(0, std2))/(player1.get_skill()+player2.get_skill()):
+            if random.random() < (player1.get_skill() + random.gauss(0, std2))\
+                    / (player1.get_skill()+player2.get_skill()):
                 return 1
             else:
                 return 2
 
         
 def convert_scores(ball_count1, ball_count2, diff):
-    '''
+    """
     Useful to display the score.
-    '''
-    game_dict = {0:0, 1:15, 2:30, 3:40}
+    """
+    game_dict = {0: 0, 1: 15, 2: 30, 3: 40}
     score1 = ''
     score2 = ''
     if ball_count1 < 4:
@@ -95,11 +103,13 @@ def convert_scores(ball_count1, ball_count2, diff):
             score2 = 'D'
     return score1, score2
 
-def play_game(p1, p2, balls, diff):
+
+def play_game(player_1, player_2, balls, diff):
     ball_count1 = 0
     ball_count2 = 0
-    while (ball_count1 <= balls and ball_count2 <= balls) or abs(ball_count1-ball_count2)< diff:
-        res = ball_result(p1, p2)
+    while (ball_count1 <= balls and ball_count2 <= balls) or \
+            abs(ball_count1-ball_count2) < diff:
+        res = ball_result(player_1, player_2)
         if res == 1:
             ball_count1 += 1
         elif res == 2:
@@ -111,29 +121,35 @@ def play_game(p1, p2, balls, diff):
     else:
         return 2
 
-def play_set(p1, p2, balls, diff, min_games, max_games, games_diff):
+
+def play_set(player_1, player_2, balls, diff, min_games, max_games,
+             games_diff):
     game_count1 = 0
     game_count2 = 0
-    while (game_count1 < min_games and game_count2 < min_games) or (abs(game_count1 - game_count2) < games_diff and game_count1 < max_games and game_count2 < max_games):
-        res = play_game(p1, p2, balls, diff)
+    while (game_count1 < min_games and game_count2 < min_games) or \
+            (abs(game_count1 - game_count2) < games_diff
+             and game_count1 < max_games and game_count2 < max_games):
+        res = play_game(player_1, player_2, balls, diff)
         if res == 1:
-            game_count1 +=1
+            game_count1 += 1
         else:
-            game_count2 +=1
+            game_count2 += 1
         #print game_count1, ' : ', game_count2
     return game_count1, game_count2
 
-def play_match(p1, p2, score):
-    '''
+
+def play_match(player_1, player_2, score):
+    """
     p1, p2 - players
     score - Score object
-    '''
+    """
     min_sets = score.min_sets
     sets = []
     sets1 = 0
     sets2 = 0
     while sets1 < min_sets and sets2 < min_sets:
-        res = play_set(p1, p2, score.balls, score.diff, score.min_games, score.max_games, score.games_diff)
+        res = play_set(player_1, player_2, score.balls, score.diff,
+                       score.min_games, score.max_games, score.games_diff)
         sets.append(res)
         if res[0] > res[1]:
             sets1 += 1
@@ -155,12 +171,12 @@ def simulation(player1, player2, score, num):
     return win_one
 
 
-def sim_ball_res(p1, p2, std1, std2, boundary, num):
+def sim_ball_res(player_1, player_2, std1, std2, boundary, num):
     res_one = 0
     res_two = 0
     res_zero = 0
     for i in range(num):
-        res = ball_result(p1, p2, std1, std2, boundary)
+        res = ball_result(player_1, player_2, std1, std2, boundary)
         if res == 1:
             res_one += 1
         elif res == 2:
